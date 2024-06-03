59befa0d048c57e0a6535648167c96300ae07125
AsumahBanda <117347715+AsumahBanda@users.noreply.github.com>
2024-06-03 12:02:47 +0000

    Initial Commit

---
+++ MaxTabApp/MaxTabAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaxTabAppApp.swift
+//  MaxTabApp
+//
+//  Created by AMALITECH-PC-593 on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MaxTabAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
